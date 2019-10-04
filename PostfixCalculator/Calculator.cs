85b6163f5d9ec29d62e7c5ec29864e1ab22fe96c
Kenan Casey <kenancasey@gmail.com>
2019-10-04 14:13:35 -0500

    iniital commit

---
+++ PostfixCalculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostfixCalculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public double Evaluate(string postfixExpression)
+        {
+            Stack<double> stack = new Stack<double>();
+
+            string[] tokens = postfixExpression.Split();
+
+            foreach( var token in tokens)
+            {
+                if (IsDouble(token))
+                {
+                    stack.Push(double.Parse(token));
+                }
+
+                // if token is an operand
+                // do something else
+
+            }
+
+            return 0.0;
+        }
+
+        private bool IsDouble(string s)
+        {
+            double result;
+
+            bool isDouble = double.TryParse(s, out result);
+          
+            return isDouble;
+        }
+    }
+}
---
+++ PostfixCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostfixCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
